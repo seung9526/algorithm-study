7a1ccd4ebeda8b54e60002433df501bc131be41f
NamNaam <86337357+NamNaam@users.noreply.github.com>
2023-04-18 15:17:15 +0900

    [BOJ] 10828 스택 | 스택

---
+++ 김남형/Week2/BOJ_10828_스택.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+
+for i in range(n):
+  com = sys.stdin.readline().split()
+  
+  if com[0] == 'push':
+    arr.append(com[1])
+  elif com[0] == 'top':
+    if len(arr) > 0:
+      print(arr[-1])
+    else:
+      print(-1)
+  elif com[0] == 'size':
+    print(len(arr))
+  elif com[0] == 'pop':
+    if len(arr) > 0:
+      print(arr.pop())
+    else:
+      print(-1)
+  elif com[0] == 'empty':
+    if len(arr) > 0:
+      print(0)
+    else:
+      print(1)
