974050ed5aaabe0dadd282d84f146a9d1c9018a4
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-05-08 23:47:18 +0900

    [BOJ] 2839 설탕배달 | DP, Greedy

---
+++ 성경민/Week5/BOJ_2839_설탕배달.java
@@ -0,0 +1,30 @@
+import java.io.*;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 0;
+		
+		while(true){
+			if(N%5==0){
+				cnt+=N/5;
+				System.out.println(cnt);
+				break;
+			} else {
+				N -=3;
+				cnt++;
+			}
+			
+			if(N<0){
+				System.out.println("-1");
+				break;
+			}
+			
+		}
+	}
+}
