b381a844ec25711ea18d4be350bf4d348c6bbf7a
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-05-15 19:38:30 +0900

    [BOJ] 14500 테트로미노 | 브루트포스

---
+++ Week6/BOJ_14500_테트로미노.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] graph;
+    static boolean[][] visited;
+    static int n, m;
+    static int ans, max;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, graph[i][j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dfs(j, i, graph[i][j], 1);
+            }
+        }
+        System.out.println(ans);
+        br.close();
+    }
+
+    static void dfs(int x, int y, int total, int cnt) {
+        if (cnt == 4) {
+            ans = Math.max(ans, total);
+            return;
+        }
+
+        if (ans >= total + max * (4 - cnt)) {
+            return;
+        }
+
+        visited[y][x] = true;
+
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+            if (visited[ny][nx]) continue;
+
+            if (cnt == 2) {
+                visited[ny][nx] = true;
+                dfs(x, y, total + graph[ny][nx], cnt + 1);
+                visited[ny][nx] = false;
+                visited[y][x] = true;
+            }
+            dfs(nx, ny, total + graph[ny][nx], cnt + 1);
+        }
+        visited[y][x] = false;
+    }
+}
